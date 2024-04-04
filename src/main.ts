18dd89b4f253fdd6792a47d94b0eede52c8f12ef
Valunchanut <valunchanut@outlook.com>
2024-04-03 20:21:00 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
+import { environment } from './app/environments/environment';
 if (environment.production) {
   enableProdMode();
 }
